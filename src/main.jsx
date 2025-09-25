ef3aa33771ebabe9f9814e15b96dca8af3861ff6
Rahul Kumar <rahulrajkumar123.rrk@gmail.com>
2025-09-26 00:47:31 +0530

    chore: Temporarily remove StrictMode for debugging

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,14 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 async function enableMocking() {
-  // Humne yahan se development check wali 'if' condition hata di hai
-  // Ab mock server hamesha start hoga
   const { worker } = await import('./api/browser.js');
   return worker.start();
 }
  
 enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   );
 });
